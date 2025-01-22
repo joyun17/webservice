72401f0eac34d93bd4adf46e64772816bc42a5f8
joyoon17 <117166331+joyoon17@users.noreply.github.com>
2025-01-22 23:33:41 +0900

    postsService, PostsSaveRequestDto, PostsApiController 생성

---
+++ src/main/java/org/example/webservice/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package org.example.webservice.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.example.webservice.domain.posts.PostsRepository;
+import org.example.webservice.web.dto.PostsSaveRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
