fe860cd7f3d0fd65f1afd3e583983163f6d26703
joyun17 <163952809+joyun17@users.noreply.github.com>
2025-01-24 19:22:47 +0900

    list 조회, 등록 ui 추가

--- src/main/java/org/example/webservice/service/posts/PostsService.java
+++ src/main/java/org/example/webservice/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package org.example.webservice.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.example.webservice.domain.posts.Posts;
 import org.example.webservice.domain.posts.PostsRepository;
+import org.example.webservice.web.dto.PostsListResponseDto;
 import org.example.webservice.web.dto.PostsResponseDto;
 import org.example.webservice.web.dto.PostsSaveRequestDto;
 import org.example.webservice.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -23,11 +27,23 @@ public class PostsService {
     public Long update(Long id, PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
+        /*
+          트랜잭션 안에서 데이터베이스에서 데이터를 가져오면 이 데이터는 영속성 컨텍스트가 유지된 상태,
+        이 상태에서는 해당 데이터의 값을 변경하면 트랜잭션이 끝나는 시점에 해당 테이블에 변경분을 반영함 으로
+        따로 update쿼리를 날릴 필요가 없음. ex) postsRepository.save(posts);
 
+        */
         return id;
     }
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
